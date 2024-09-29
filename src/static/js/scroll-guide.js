d9ceb158b923701cdc7382e93e0266567716978b
GG99-99 <jeremy0Titan@gmail.com>
2024-09-28 21:59:31 -0400

    28/9/24

--- src/static/js/scroll-guide.js
+++ src/static/js/scroll-guide.js
@@ -1,13 +1,14 @@
 
+const scrollGuideCont = document.querySelector(".scroll-guide_grand-father-cont");
 
-const scrollGuide = document.querySelectorAll(".scroll-guide-cont__scroll-guide");
-const innerCircleGuide = ' <div class="scroll-guide-cont__scroll-guide__circle"></div>';
+const scrollGuide = document.querySelectorAll(".scroll-guide-cont__scroll-guide__circle");
+
 const header = document.querySelector("header");
 const aboutCont = document.querySelector(".about-cont")
 const article1 = document.querySelector(".edu-cont");
 
 
-scrollGuide[0].innerHTML = innerCircleGuide
+
 scrollGuide[0].classList.add("scale1")
 
 let i;
@@ -16,7 +17,6 @@ function circleAndScale(i, list){
     for (let id in list){
         if (id == i){
             list[id].classList.add("scale1");
-            list[id].innerHTML = innerCircleGuide;
             return
         }
     }
@@ -24,22 +24,39 @@ function circleAndScale(i, list){
 
 function removeCircleScale(e1, e2, list){
     list[e1].classList.remove("scale1");
-    list[e1].innerHTML ="";
+    
 
     list[e2].classList.remove("scale1");
-    list[e2].innerHTML ="";
+    
 }
 
+var lastScrollTop = window.scrollY /* that var is for hide de scroll guide */
 
+function hideScrollGuide(lst, cont) {
+    let scrollFromTop = window.scrollY 
+
+    if (scrollFromTop > lst ){
+        cont.style.top = "-100px"     
+    } 
+    
+    else {
+        cont.style.top= "0px"
+    }
+
+    lastScrollTop = scrollFromTop
+}
 
 
 window.addEventListener('scroll', 
 function () {    // esta es la funcion que toma los top del header, article ...
+
+   
+
     let headerTop = header.getBoundingClientRect().top;
     let aboutContTop = aboutCont.getBoundingClientRect().top;
     let article1Top = article1.getBoundingClientRect().top;
     let windowHeight = window.innerHeight / 3
-
+    
     
     /* --- circle 1  --- */
     if (headerTop  < windowHeight){
@@ -67,11 +84,13 @@ function () {    // esta es la funcion que toma los top del header, article ...
     
     }
     
-    
+    hideScrollGuide(lastScrollTop, scrollGuideCont);
 
 }
 
 );
+
+
 
 
 
