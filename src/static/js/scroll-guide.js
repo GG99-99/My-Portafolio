4f19e7767f567a12162b4ca2c1df1755145560b5
Jeremy Rafael <jeremy0Titan@gmail.com>
2025-12-29 23:04:17 -0400

    se mejoro el scroll guide implemento IntersectionObserver

--- src/static/js/scroll-guide.js
+++ src/static/js/scroll-guide.js
@@ -1,95 +1,53 @@
 
 const scrollGuideCont = document.querySelector(".scroll-guide-region");
-
 const scrollGuides = document.querySelectorAll(".circle");
 
-const header = document.querySelector("header");
-const aboutCont = document.querySelector(".about")
-const article1 = document.querySelector(".section_article");
-const article2 = document.querySelector("#proyectos");
-
-
-scrollGuides[0].classList.add("scale1")
-
-// let i;
-
-
+const conts = [
+    document.querySelector("header"),
+    document.querySelector("#about"),
+    document.querySelector("#educacion"),
+    document.querySelector("#proyectos")
+]
 
 function CircleScale(actualCircleNum){  
     // agrega escala al circulo indicada y limpia a los demas
 
     for (let i = 0; i < scrollGuides.length; i++){
-        if (i != actualCircleNum){
-            scrollGuides[i].classList.remove("scale1");
-        }
-
-        if(i == actualCircleNum){
-            scrollGuides[i].classList.add("scale1");
-        }
+        scrollGuides[i].classList.toggle("scale1", i == actualCircleNum)
     }
     
 }
 
-var lastScrollTop = window.scrollY /* that var is for hide de scroll guide */
-
-function hideScrollGuide(cont) {
-    let scrollFromTop = window.scrollY 
+var lastScrollTop = window.scrollY/* that var is for hide de scroll guide */
+function hideScrollGuide() {
+    let scrollFromTop = window.scrollY
 
     if (scrollFromTop > lastScrollTop ){
-        cont.style.top = "-100px"     
+        scrollGuideCont.style.top = "-100px"     
     } 
     
     else {
-        cont.style.top= "0px"
+        scrollGuideCont.style.top= "0px"
     }
 
-    lastScrollTop = scrollFromTop
+    lastScrollTop = scrollFromTop 
 }
 
-function mngScrollGuide() {    // esta es la funcion que toma los top del header, article ...
+let observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            let index = conts.indexOf(entry.target)
+            CircleScale(index)
+        }
+    })
+}, {threshold: 0.55})
 
-   
-
-    let headerTop = header.getBoundingClientRect().top;
-    let aboutContTop = aboutCont.getBoundingClientRect().top;
-    let article1Top = article1.getBoundingClientRect().top;
-    let article2Top = article2.getBoundingClientRect().top;
-
-    let windowHeight = window.innerHeight / 3
-    
-    
-    /* --- circle 0  --- */
-    if (headerTop  < windowHeight){
-        CircleScale(0)
-    }
-    
-
-
-    /* --- circle 1 --- */
-
-    if (aboutContTop < windowHeight){
-        CircleScale(1)
-    }
-    
-
-
-    /* --- circle 3 --- */
-    if (article1Top < windowHeight){
-        CircleScale(2)
-    }
-    
-
-    /* --- circle 4 --- */
-    if (article2Top < windowHeight){
-        CircleScale(3)
-    }
-
-    
-    hideScrollGuide(scrollGuideCont);
-
+for(let cont of conts){
+    observer.observe(cont)
 }
 
-window.addEventListener('scroll', mngScrollGuide);
+
+window.addEventListener('scroll', hideScrollGuide);
 
 
 
